4e8ce1c4286aacd92f638034f86b2054a027ab2e
Raime-34 <55358674+Raime-34@users.noreply.github.com>
2021-10-15 12:55:51 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -3,17 +3,45 @@ const Distube = require('distube');
 const {resolveSpeechWithGoogleSpeechV2} = require('discord-speech-recognition');
 const client = new Discord.Client();
 const {token} = require('./config.json');
-const distube = new Distube(client, {searchSongs: false, emitNewSongOnly: true});
+const distube = new Distube(client, {searchSongs: true, emitNewSongOnly: true});
 const {prefix} = require('./config.json');
 const {music_channel} = require('./config.json');
+const {reserved} = require('./config.json');
+const { DisTubeOptions } = require('distube');
+const validator = require('youtube-validator');
+const { validateUrl } = require('youtube-validator');
 client.login(token);
 
 client.on('ready', () =>{
     console.log('Опять работать');
 });
 
+distube.on("searchResult", (message, results) => {
+    message.channel.send(`**Choose an option from below**\n${results.map((song, i) => `**${i + 1}**. ${song.name} - \`${song.formattedDuration}\``).join("\n")}\n*Enter anything else or wait 60 seconds to cancel*`);
+});
+
 client.on('message', msg=>{
     if(msg.author.bot) return;
+    
+    reserved.forEach(element => {
+        if(element.name === msg.content.toLowerCase().trim()){
+            distube.play(msg, element.url);
+            return;
+        }
+    });
+
+    validator.validateUrl(msg.content, function(res, err) {
+        if(err) //err
+        {
+            msg.channel.send(msg.content);
+            msg.channel.send(err);
+        }
+        else
+          {
+            msg.channel.send("video url is correct");
+         }
+      });
+
     if(!msg.content.startsWith(prefix)) return;
     if(!msg.channel.name === music_channel) return;
     // if(!msg.author.voice){
@@ -25,50 +53,57 @@ client.on('message', msg=>{
         command = msg.content;  
     else command = msg.content.substr(0, devider);
 
-    switch(command){
-        case '/play':
-            distube.play(msg, msg.content.replace(command, '').trim());
-            break;
-
-        case '/stop':
-            distube.stop(msg);
-            break;
-
-        case '/pause':
-            distube.pause(msg);
-            break;
-
-        case '/skip':
-            distube.skip(msg);
-            break;
-
-        case '/!repeat':
-            distube.setRepeatMode(msg, 0);
-            msg.channel.send('Должен был выключить Repeat Mod');
-            break;
-
-        case '/repeat':
-            distube.setRepeatMode(msg, 1);
-            msg.channel.send('Должен был включить Repeat Mod для конкретной песни');
-            break;
-        
-        case '/repeatQ':
-            distube.setRepeatMode(msg, 2);
-            msg.channel.send('Должен был включить Repeat Mod для всего плейлиста');
-            break; 
+    try {
+        switch(command){
+            case '/play':
+                distube.play(msg, msg.content.replace(command, '').trim());
+                break;
+    
+            case '/stop':
+                distube.stop(msg);
+                break;
+    
+            case '/pause':
+                distube.pause(msg);
+                break;
+    
+            case '/skip':
+                distube.skip(msg);
+                break;
+    
+            case '/!repeat':
+                distube.setRepeatMode(msg, 0);
+                break;
+    
+            case '/repeat':
+                distube.setRepeatMode(msg, 1);
+                break;
             
-        case '/cyka':
-            msg.channel.send('Я должен был говорить сука без остановки', {tts: true});
-            var i = 0;
-            while(i < 5){
-                msg.channel.send('СУКА', {tts: true});
-                i++;
-            }
-            break;
+            case '/repeatQ':
+                distube.setRepeatMode(msg, 2);
+                break; 
+                
+            case '/cyka':
+                msg.channel.send('Ебанный рот этого казино, блять', {tts: true});
+                break;
+    
+            case '/join':
+                msg.member.voice.channel.join();
+                break;
+    
+            case '/showQ':
+                let Q = distube.getQueue(msg);
+                let line = '```' + Q.songs.map((song, id) => (id + 1) + '. ' + song.name).join('\n') + '```';
+                msg.channel.send('Текущая очередь:\n' + line);
+                break;
 
-        case '/join':
-            msg.member.voice.channel.join();
-            break;
+            case '/Q':
+                let q = distube.search(msg.content.replace(command, '').trim());
+                q.then((value) => {msg.channel.send(value.url + '\n')});
+                break;
+        }
+    } catch (MinigetError) {
+        msg.channel.send(MinigetError.message);
     }
 
     client.on("speech", (msg) => {
